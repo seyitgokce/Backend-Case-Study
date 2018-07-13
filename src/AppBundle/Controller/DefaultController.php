b1a5c9ee53657edb1fec0dc5cb11f42c27fd4e0b
Seyit Ugur GOKCE <seyitgokce@gmail.com>
2018-07-13 18:22:25 +0300

    :-1:
    Çözülmesi gereken hatalar var!

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Currency;
 
 class DefaultController extends Controller
 {
@@ -13,6 +14,9 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+
+
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
